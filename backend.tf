c68b3631ea33c5639c9c75bc78da461a68b6c705
James Fenn <me@jfenn.me>
2025-11-19 13:33:12 +0000

    fix s3 backend credentials not being passed through

--- backend.tf
+++ backend.tf
@@ -10,10 +10,12 @@ terraform {
     bucket                      = "tofu-remote-state"
     key                         = local.env_key_map[var.env]
     region                      = "us-east"
-    profile                     = "fastly-us-east"
+    access_key                  = var.aws_access_key_id
+    secret_key                  = var.aws_secret_access_key
     skip_credentials_validation = true
     skip_region_validation      = true
     use_path_style              = true
+    use_lockfile                = true
     endpoints = {
       s3 = "https://us-east.object.fastlystorage.app"
     }
--- variables.tf
+++ variables.tf
@@ -25,3 +25,11 @@ variable "porkbun_api_key" {
 variable "porkbun_secret_key" {
   type = string
 }
+
+variable "aws_access_key_id" {
+  type = string
+}
+
+variable "aws_secret_access_key" {
+  type = string
+}
